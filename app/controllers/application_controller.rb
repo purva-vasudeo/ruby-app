1be68b28f4244909a71d2a1c8a3b2be6851e4dbc
Your Name <you@example.com>
2021-04-20 11:58:59 -0700

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def hello
+    render html: "Hello, world from Azure App Service on Linux!"
+  end
+
+  def robot
+    render html: "Hello, robot!"
+  end
 end
